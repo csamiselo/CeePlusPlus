7c50ed77182a71f387e0fa83791c5a54529c94d0
csamiselo <chisambwe.samiselo@gmail.com>
2020-07-15 11:57:57 +0200

    For statement,symbolic constants and chararcter streams

--- Basics/tempconv2.c
+++ Basics/tempconv2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 //Program to convert degrees centigrade to farenheit
-main(){
+main()
+{
     float fareinheit;
     float celsius;
-    int lower,upper,step;
-     
-    lower = 0;//temparature lower limit
-    upper = 100;//temp upper limit
-    step = 20;//step size
+    int lower, upper, step;
+
+    lower = 0;   //temparature lower limit
+    upper = 100; //temp upper limit
+    step = 10;   //step size
 
     celsius = lower;
     while (celsius <= upper)
     {
-        fareinheit = (celsius * 9) + (160) / 9; 
+        fareinheit = (celsius * 9) + (160) / 9;
         printf("Temperature conversion Chart from Celsius to degrees Farenheit");
-        printf("%3.0f %6.1f\n",fareinheit,celsius);
+        printf("%3.0f %6.1f\n", fareinheit, celsius);
         fareinheit = fareinheit + step;
     }
-
 }
